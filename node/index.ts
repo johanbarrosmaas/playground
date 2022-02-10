f67cb537a2939cf6d55891876ad18eeb460c62f7
Diney <diney@saude.sc.gov.br>
2022-02-10 13:24:43 -0300

    Validação de cnpj e cpf

--- node/index.ts
+++ node/index.ts
@@ -1,9 +1,14 @@
-import { endereco, cidadeByCep } from "./enderecos";
+////import { endereco, cidadeByCep } from "./enderecos";
+
+import { verificaDoc, removeMascara,mascaraCnpj,mascaraCpf} from "./cnpj-cpf";
 
 
 
-endereco('89051-300').then(endereco => {
-    console.log(`Cidade retornada:`, [endereco.cidade]);
-});
 
-console.log(cidadeByCep('Blumenau'));
+//endereco('89051-300').then(endereco => {
+   // console.log(`Cidade retornada:`, [endereco.cidade]);
+//});
+
+//console.log(cidadeByCep('Blumenau'));
+
+console.log( verificaDoc('123456789012349'));
